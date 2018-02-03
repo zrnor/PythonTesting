709525521f9ec63c7a8e288740cea647d94b9955
zrnor <36115308+zrnor@users.noreply.github.com>
2018-02-03 17:01:02 -0500

    Delete TempConvert.py

--- TempConvert.py
+++
@@ -1,6 +0,0 @@
-print 'Fahrenheit to Celsius converter'
-tempF = input("Enter temperature in Fahrenheit: ")
-
-tempC = (tempF - 32.0) * (5.0/9)
-print ("%.2f degrees Fahrenheit is %.2f degrees Celsius" % (tempF, tempC))
-
