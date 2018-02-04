c2b45fd8b6785f7e5c5e97ccdc078ebfcab3627d
zrnor <36115308+zrnor@users.noreply.github.com>
2018-02-04 00:57:36 -0500

    Made the tempconvert much better
    Made it "error resistant" and added a quit option. If you can find a way to break it, let me know.

--- TempConvert/TempConvert.py
+++ TempConvert/TempConvert.py
@@ -6,11 +6,11 @@ while True:
         if tempF == "quit":
             print "Shutting Down."
             raise SystemExit
-
         else:
             tempF = float(tempF)
             tempC = (tempF - 32.0) * (5.0/9)
             print ("%.2f degrees Fahrenheit is %.2f degrees Celsius." % (tempF, tempC))
+   
     except (AttributeError, NameError,ValueError):
         print "Error: Not a valid number."
     
