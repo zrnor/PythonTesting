b5996e517134fe241deca00dd8cfb67a827829b0
zrnor <36115308+zrnor@users.noreply.github.com>
2018-02-04 00:56:02 -0500

    Update TempConvert.py

--- TempConvert/TempConvert.py
+++ TempConvert/TempConvert.py
@@ -1,5 +1,16 @@
 print 'Fahrenheit to Celsius converter'
-tempF = input("Enter temperature in Fahrenheit: ")
+while True:
+    try:
+        tempF = raw_input("Enter temperature in Fahrenheit: ")
 
-tempC = (tempF - 32.0) * (5.0/9)
-print ("%.2f degrees Fahrenheit is %.2f degrees Celsius" % (tempF, tempC))
+        if tempF == "quit":
+            print "Shutting Down."
+            raise SystemExit
+
+        else:
+            tempF = float(tempF)
+            tempC = (tempF - 32.0) * (5.0/9)
+            print ("%.2f degrees Fahrenheit is %.2f degrees Celsius." % (tempF, tempC))
+    except (AttributeError, NameError,ValueError):
+        print "Error: Not a valid number."
+    
