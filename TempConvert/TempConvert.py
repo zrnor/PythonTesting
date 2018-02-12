b365b5e561a7524c6c0dbb0a18c45e8b5ec6d733
zrnor <36115308+zrnor@users.noreply.github.com>
2018-02-11 20:35:34 -0500

    Added C or F query

--- TempConvert/TempConvert.py
+++ TempConvert/TempConvert.py
@@ -1,16 +1,30 @@
 print 'Fahrenheit to Celsius converter'
 while True:
     try:
-        tempF = raw_input("Enter temperature in Fahrenheit: ")
+        firstquery = raw_input("Convert to Celsius or Fahrenheit? [C/F]: ")
+        firstquery = firstquery.lower()
+        if firstquery == "f":
+            tempCPrint = raw_input("Enter temperature in Celsius: ")
 
-        if tempF == "quit":
-            print "Shutting Down."
-            raise SystemExit
+            if tempCPrint == "quit":
+                print "Shutting Down."
+                raise SystemExit
+            else:
+                tempCPrint = float(tempCPrint)
+                tempFPrint = (1.8 * tempCPrint + 32)
+                print ("%.2f degrees Celsius is %.2f degrees Fahrenheit." % (tempCPrint,tempFPrint))
+        elif firstquery == "c":
+            tempF = raw_input("Enter temperature in Fahrenheit: ")
+
+            if tempF == "quit":
+                print "Shutting Down."
+                raise SystemExit
+            else:
+                tempF = float(tempF)
+                tempC = (tempF - 32.0) * (5.0/9)
+                print ("%.2f degrees Fahrenheit is %.2f degrees Celsius." % (tempF, tempC))
         else:
-            tempF = float(tempF)
-            tempC = (tempF - 32.0) * (5.0/9)
-            print ("%.2f degrees Fahrenheit is %.2f degrees Celsius." % (tempF, tempC))
-   
+            print "Error: Input must be C or F."
     except (AttributeError, NameError,ValueError):
         print "Error: Not a valid number."
     
